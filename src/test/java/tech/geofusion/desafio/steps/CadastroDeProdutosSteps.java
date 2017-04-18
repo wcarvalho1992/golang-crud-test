68c37d1000243024c9c0d84a83115107aabe7280
wcarvalho1992 <washington.arljj@gmail.com>
2017-04-18 13:43:19 -0300

    Refactor to organize the code in Given Step method and avoid replication.

--- src/test/java/tech/geofusion/desafio/steps/CadastroDeProdutosSteps.java
+++ src/test/java/tech/geofusion/desafio/steps/CadastroDeProdutosSteps.java
@@ -15,7 +15,7 @@ import tech.geofusion.desafio.support.domain.Product;
 public class CadastroDeProdutosSteps {
 
     private static final String USER_NAME = "wcarvalho";
-    private static final String BROWSER_NAME = "firefox";
+    private static final String BROWSER_NAME = "chrome";
     private WebDriver driver;
     private CrudPage crudPage;
 
@@ -26,11 +26,11 @@ public class CadastroDeProdutosSteps {
 
     @Dado("^que estou na tela de cadastro de novo produto$")
     public void que_estou_na_tela_de_cadastro_de_novo_produto() throws Throwable {
+        abrirTelaDeCadastro();
     }
 
     @Quando("^eu submeter o formulário preenchido com os dados válidos$")
     public void eu_submeter_o_formulário_preenchido_com_os_dados_válidos() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().build();
         efetuarCadastro(product);
     }
@@ -42,7 +42,6 @@ public class CadastroDeProdutosSteps {
 
     @Quando("^eu submeter o formulário com o campo Nome do Produto vazio$")
     public void eu_submeter_o_formulário_com_o_campo_Nome_do_Produto_vazio() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withName("").build();
         efetuarCadastro(product);
     }
@@ -54,21 +53,18 @@ public class CadastroDeProdutosSteps {
 
     @Quando("^eu submeter o formulário com o campo Preço vazio$")
     public void eu_submeter_o_formulário_com_o_campo_Preço_vazio() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withPrice("").build();
         efetuarCadastro(product);
     }
 
     @Quando("^eu submeter o formulário com o campo Data de validade vazio$")
     public void eu_submeter_o_formulário_com_o_campo_Data_de_validade_vazio() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withExpirationDate("").build();
         efetuarCadastro(product);
     }
 
     @Quando("^eu submeter o formulário excedendo o limite de caracteres do campo Nome do Produto$")
     public void eu_submeter_o_formulário_excedendo_o_limite_de_caracteres_do_campo_Nome_do_Produto() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withName("testetestetestetestetestetestetestetestetestetesteteste").build();
         efetuarCadastro(product);
     }
@@ -80,7 +76,6 @@ public class CadastroDeProdutosSteps {
 
     @Quando("^eu submeter o formulário com a data de validade anterior a presente data$")
     public void eu_submeter_o_formulário_com_a_data_de_validade_anterior_a_presente_data() throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withExpirationDate("before today").build();
         efetuarCadastro(product);
     }
@@ -88,7 +83,6 @@ public class CadastroDeProdutosSteps {
     @Quando("^eu submeter o formulário com o campo Preço preenchido com um valor diferente do formato de moeda$")
     public void eu_submeter_o_formulário_com_o_campo_Preço_preenchido_com_um_valor_diferente_do_formato_de_moeda()
             throws Throwable {
-        abrirTelaDeCadastro();
         Product product = new ProductBuilder().withPrice("10").build();
         efetuarCadastro(product);
     }
